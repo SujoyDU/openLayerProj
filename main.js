af0aca8463b2684201454437b0cc3b70648935e5
sujoyDU <debnath.sujoy@hotmail.com>
2020-02-17 00:33:56 -0500

    changes

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ window.onload = init;
 function init(){
     const map = new ol.Map({
         view: new ol.View({
-            center: [0,0],
-            zoom: 3
+            center: [-12080385,7567433],
+            zoom: 3,
+            maxZoom: 7,
+            minZoom: 1,
+            
         }),
         layers: [
             new ol.layer.Tile({
@@ -11,8 +14,7 @@ function init(){
 
             })
         ],
-        target: 'js-map'
-        
+        target: 'js-map'      
     })
    
 }
