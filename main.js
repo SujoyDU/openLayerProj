523c940e5a912e3124e7cc58ae76af1154e3dac9
sujoyDU <debnath.sujoy@hotmail.com>
2020-04-04 12:39:36 -0400

    adding controls

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 window.onload = init;
+const fullScreenControl = new ol.control.FullScreen();
+const mousePosititionControl = new ol.control.MousePosition();
+const overviewMapControl = new ol.control.OverviewMap({
+    collapsed: true,
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+
+        })
+    ]
+});
+const scaleLineControl = new ol.control.ScaleLine();
+const zoomSliderControl = new ol.control.ZoomSlider();
+const zoomToExtentControl = new ol.control.ZoomToExtent();
+
 function init(){
     const map = new ol.Map({
         view: new ol.View({
             center: [-12080385,7567433],
             zoom: 3,
-            maxZoom: 7,
+            maxZoom: 6,
             minZoom: 1,
-            
+            rotation: 0
         }),
         layers: [
             new ol.layer.Tile({
@@ -14,9 +29,17 @@ function init(){
             })
         ],
         target: 'js-map',      
-        keyboardEventTarget: document
+        keyboardEventTarget: document,
+        controls: ol.control.defaults().extend([
+            fullScreenControl,
+            mousePosititionControl,
+            overviewMapControl,
+            scaleLineControl,
+            zoomSliderControl,
+            zoomToExtentControl
+        ])
     })
-
+    console.log(ol.control.defaults())
     const popupContainerElement = document.getElementById('popup-coordinates');
     const popup = new ol.Overlay({
         element: popupContainerElement,
@@ -25,6 +48,7 @@ function init(){
    
     map.addOverlay(popup);
 
+    
     map.on('click', function(e) {
         const clickedCoordinate = e.coordinate;
         popup.setPosition(undefined);
@@ -32,6 +56,7 @@ function init(){
         popupContainerElement.innerHTML = clickedCoordinate;
 
     })
+    
 
     //dragRotate Interaction
     const dragRotateInteraction = new ol.interaction.DragRotate({
